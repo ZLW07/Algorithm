30465a760440ec341849321602d8e312de6c94e3
wei <205430509@qq.com>
2022-10-12 21:23:22 +0800

    fix(use grid map)

--- Dijkstra/DijkstraGridMap.py
+++ Dijkstra/DijkstraGridMap.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import math
 import copy
-from matplotlib.pyplot import MultipleLocator
 import numpy as np
 import sys
 
@@ -33,7 +32,7 @@ class PlotMap:
         self.end = end_point
 
         self.OpenList = dict()
-        self.CloseList = dict()
+        self.CloseList = []
 
         self.Open = []
 
@@ -64,7 +63,7 @@ class PlotMap:
         for ij in np.arange(min_y, max_y - 10):
             self.border_point.append([-5, ij])
 
-        for ij in np.arange(max_y - 20, max_y):
+        for ij in np.arange(max_y - 15, max_y):
             self.border_point.append([5, ij])
 
         self.map_point = dict()
@@ -85,45 +84,74 @@ class PlotMap:
     def CalculateIndex(self, x, y):
         return (self.maxX - self.minX + 1) * (x - self.minX) + (y - self.minY)
 
-    def Motion(self, xy):
-        OpenList = []
-        for id in xy:
-            current_x = id[0]
-            current_y = id[1]
+    def Dijkstra(self, xy):
+        for PointPosition in xy:
+            current_x = PointPosition[0]
+            current_y = PointPosition[1]
             init_node = Node()
             for motion_x, motion_y, cost in self.motion:
-                if (current_x + motion_x) == self.end[0] and (current_y + motion_y) == self.end[1]:
+                target_x = current_x + motion_x
+                target_y = current_y + motion_y
+                if target_x == self.end[0] and target_y == self.end[1]:
                     point_index_before = self.CalculateIndex(current_x, current_y)
-                    print("-----: ", self.map_point[point_index_before].cost)
                     init_node.cost = cost + self.map_point[point_index_before].cost
                     init_node.father_index = [current_x, current_y]
-
-                    point_index_current = self.CalculateIndex(current_x + motion_x, current_y + motion_y)
+                    point_index_current = self.CalculateIndex(target_x, target_y)
                     if self.map_point[point_index_current].cost > init_node.cost:
                         self.map_point[point_index_current] = copy.deepcopy(init_node)
                     return
 
-                if self.Open.count([current_x + motion_x, current_y + motion_y]) == 0:
-                    if self.border_point.count([current_x + motion_x, current_y + motion_y]) == 0:
-                        self.Open.append([current_x + motion_x, current_y + motion_y])
-                        OpenList.append([current_x + motion_x, current_y + motion_y])
-
+                if self.Open.count([target_x, target_y]) == 0:
+                    if self.border_point.count([target_x, target_y]) == 0:
+                        if target_x != self.start[0] or target_y != self.start[1]:
+                            self.Open.append([target_x, target_y])
                         point_index_before = self.CalculateIndex(current_x, current_y)
-                        print("-----: ", self.map_point[point_index_before].cost)
                         init_node.cost = cost + self.map_point[point_index_before].cost
                         init_node.father_index = [current_x, current_y]
-
-                        point_index_current = self.CalculateIndex(current_x + motion_x, current_y + motion_y)
+                        point_index_current = self.CalculateIndex(target_x, target_y)
                         if self.map_point[point_index_current].cost > init_node.cost:
                             self.map_point[point_index_current] = copy.deepcopy(init_node)
+        self.Dijkstra(self.Open)
 
-                        plt.scatter(current_x + motion_x, current_y + motion_y, c='g')
-                        # if len(self.Open) % 5 == 0:
-                        #     plt.pause(0.001)
+    def GetPath(self):
+        EndIndex = self.CalculateIndex(endPoint[0], endPoint[1])
+        self.CloseList.append([endPoint[0], endPoint[1]])
+        dd = self.map_point[EndIndex].father_index
+        self.CloseList.insert(0, dd)
+        index = self.CalculateIndex(dd[0], dd[1])
+        while 1:
+            cc = self.map_point[index].father_index
+            self.CloseList.insert(0, cc)
+            if cc[0] == self.start[0] and cc[1] == self.start[1]:
+                return
+            index = self.CalculateIndex(cc[0], cc[1])
 
-                        # plt.pause(0.001)
+    def BuildingBoundaryMap(self):
+        for ij in self.border_point:
+            plt.scatter(ij[0], ij[1], c='k', marker="x")
+        plt.scatter(self.start[0], self.start[1], c='b', marker="o")
+        plt.scatter(self.end[0], self.end[1], c='b', marker="o")
 
-        self.Motion(OpenList)
+    def PlotCalculationProcess(self):
+        plt.figure(figsize=(8, 8))
+        self.BuildingBoundaryMap()
+        iCount = 0
+        for Position in self.Open:
+            iCount = iCount + 1
+            if iCount % 30 == 0:
+                plt.pause(0.01)
+            plt.plot(Position[0], Position[1], c='g', marker='o')
+
+        self.GetPath()
+        rx = []
+        ry = []
+        for ResolutionWay in self.CloseList:
+            rx.append(ResolutionWay[0])
+            ry.append(ResolutionWay[1])
+        plt.plot(rx, ry, c='r', marker='*')
+        plt.pause(1)
+        plt.grid()
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -131,28 +159,6 @@ if __name__ == '__main__':
     endPoint = [15, 15]
     oPlotMap = PlotMap(-20, -20, 20, 20, startPoint, endPoint)
     oPlotMap.Init()
-    plt.figure(figsize=(8, 8))
-    q = []
-    q.append(startPoint)
-    for ij in oPlotMap.border_point:
-        plt.scatter(ij[0], ij[1], c='k', marker="x")
-
-    # x_major_locator = MultipleLocator(1)
-    # y_major_locator = MultipleLocator(1)
-    # ax = plt.gca()
-    # ax.xaxis.set_major_locator(x_major_locator)
-    plt.xlim(-30, 30)
-    plt.ylim(-30, 30)
-    # print(oPlotMap.CalculateIndex(-19, -20))
-    plt.scatter(startPoint[0], startPoint[1], c='k', marker="x")
-    plt.scatter(endPoint[0], endPoint[1], c='b', marker="x")
-    oPlotMap.Motion(q)
-    # for dd in oPlotMap.Open:
-    #     plt.scatter(dd[0], dd[1], c='g')
-
-    for key, value in oPlotMap.map_point.items():
-        print("key: %s, value: %s", key, value)
-    # plt.pause(0.001)
-    print(oPlotMap.CalculateIndex(endPoint[0], endPoint[1]))
-    plt.grid()
-    plt.show()
+    Start = [startPoint]
+    oPlotMap.Dijkstra(Start)
+    oPlotMap.PlotCalculationProcess()
